ee37b5f77fe22dab208c5f9e5d95f3242f193d57
Aayush Ingale <aayushingale2006@gmail.com>
2025-09-25 14:47:58 -0400

    Delete M02-String-Slicing directory

--- M02-String-Slicing/Aayush-M02-string-slicing.py
+++
@@ -1,8 +0,0 @@
-##
-#Write a program that prompts the user for a string and two integers,
-#then prints a slice of the string using the two integers as the starting and end positions.
-userString = input("Enter a string: ")
-startingPosition = int(input("Enter a starting position: "))
-endingPosition = int(input("Enter a ending position: "))
-
-print("Here is the sliced string:",userString[startingPosition:endingPosition])
