c8b625752f45026cde1685beb27ae353a53212f3
Aayush Ingale <aayushingale2006@gmail.com>
2025-09-25 14:45:53 -0400

    Delete M01-Translatorphrases directory

--- M01-Translatorphrases/main.py
+++
@@ -1,9 +0,0 @@
-print("English          German")
-print()
-print("Good Morning     Guten Morgen")
-
-print("It is a pleasure to meet you    Es freut mich, Sie kennenzulernen")
-
-print("Please call me tomorrow     Ruf mich bitte morgen an")
-
-print("Have a nice day   Schönen Tag")
